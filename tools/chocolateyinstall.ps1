0432e96e6788ee1de52fc5fd8b9442a0eeccbd0b
Mick Grove <mick.grove@gmail.com>
2018-05-28 08:32:28 -0700

    adding v1.8.4

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'Lazarus'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2032%20bits/Lazarus%201.8.0/lazarus-1.8.0-fpc-3.0.4-win32.exe/download'
-$url64      = 'https://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2064%20bits/Lazarus%201.8.0/lazarus-1.8.0-fpc-3.0.4-win64.exe/download'
+$url        = 'https://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2032%20bits/Lazarus%201.8.4/lazarus-1.8.4-fpc-3.0.4-win32.exe/download'
+$url64      = 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2064%20bits/Lazarus%201.8.4/lazarus-1.8.4-fpc-3.0.4-win64.exe/download'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,9 +15,9 @@ $packageArgs = @{
 
   softwareName  = 'Lazarus*'
 
-  checksum      = '6A7684F64E63075EA6B39A8D6259F16FD160EAE8C5B59BB9EF72C7A8F4DFFF61'
+  checksum      = '2606898f3ab52379762fd647ea24b46866dcc248cca88207f355804a880528a1'
   checksumType  = 'sha256'
-  checksum64    = 'D72BCB557748D5F7E7E37AED912AC6DEE3386014469F7484544F1D0A753A7AD3'
+  checksum64    = '96eccd0e7795989413252c5828bf2a5c8105acc364e362dbc980ae2ffef95f3a'
   checksumType64= 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
