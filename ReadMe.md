7e0e3a27287bd34de5a6834a4a01086d1a9eca70
Mick Grove <mick.grove@gmail.com>
2019-03-01 13:29:43 -0800

    updated to Lazarus v2.0.0

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,3 @@
 ﻿## Description
 
-This is the chocolatey files I used to create the Lazarus chocolatey installation package. At the time of this writing, I used this to package Lazarus v1.6.2 (32-bit and 64-bit) for Windows.
+These are the chocolatey files I used to create the Lazarus chocolatey installation package. At the time of this writing, I used this to package Lazarus v2.0.0 (32-bit and 64-bit) for Windows.
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'Lazarus'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2032%20bits/Lazarus%201.8.4/lazarus-1.8.4-fpc-3.0.4-win32.exe/download'
-$url64      = 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2064%20bits/Lazarus%201.8.4/lazarus-1.8.4-fpc-3.0.4-win64.exe/download'
+$url        = 'https://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2032%20bits/Lazarus%202.0.0/lazarus-2.0.0-fpc-3.0.4-win32.exe/download'
+$url64      = 'https://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2064%20bits/Lazarus%202.0.0/lazarus-2.0.0-fpc-3.0.4-win64.exe/download'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,9 +15,9 @@ $packageArgs = @{
 
   softwareName  = 'Lazarus*'
 
-  checksum      = '2606898f3ab52379762fd647ea24b46866dcc248cca88207f355804a880528a1'
+  checksum      = 'FB64BE2210C3583F3A184A4A3ACC2342624582F7D946AB06D4FAF64C81E0326D'
   checksumType  = 'sha256'
-  checksum64    = '96eccd0e7795989413252c5828bf2a5c8105acc364e362dbc980ae2ffef95f3a'
+  checksum64    = 'E53F18E15FF94998CDA968325214D11326F20F148B47F349230151DBFC11CA70'
   checksumType64= 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
